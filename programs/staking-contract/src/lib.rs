a96996ccf54e428ed535ea2a2009d606dce33b84
quansera.dane@gmail.com <monsterdev89>
2025-01-29 12:56:59 -0500

    feat: native codin structure

--- programs/staking-contract/src/lib.rs
+++ programs/staking-contract/src/lib.rs
@@ -8,8 +8,8 @@ use anchor_spl::{
     associated_token::AssociatedToken,
     metadata::{
         mpl_token_metadata::types::DataV2
-    }
-}
+    },
+};
 
 declare_id!("8aspyEwJyJWnWffoCN36iA5asxXKi1aNjwbTQyQazBDs");
 
@@ -17,15 +17,21 @@ declare_id!("8aspyEwJyJWnWffoCN36iA5asxXKi1aNjwbTQyQazBDs");
 pub mod staking_contract {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 
-    pub fn create
+    pub fn create_token(ctx: Context<CreateToken>, decimals: u8, amount: u64) -> Result<()> {
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
 
-
+#[derive(Accounts)]
+pub struct CreateToken <'info> {
+    #[account(mut)]
+    pub mint_token: Signer<'info>
+}
 
